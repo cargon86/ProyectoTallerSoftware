d7188d896e01f35269e9210dc22f0ced294bee46
cargon86 <carlos86gonzalez68@outlook.com>
2024-10-09 17:28:29 -0600

    Agregué carpetas para cada módulo y sus respectivas pantallas

---
+++ ProyectoTallerSoftware/Modulos/Hoja_Requisicion/Hoja_Requisicion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoTallerSoftware.Modulos.Hoja_Requisicion
+{
+    public partial class Hoja_Requisicion : Form
+    {
+        public Hoja_Requisicion()
+        {
+            InitializeComponent();
+        }
+    }
+}
