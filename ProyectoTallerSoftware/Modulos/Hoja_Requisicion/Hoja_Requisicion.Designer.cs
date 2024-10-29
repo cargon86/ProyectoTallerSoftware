468cdd505b0adccac61e79a247f6904c1d94a0bb
jodanielrm <jsdnlrdrgz@gmail.com>
2024-10-28 20:46:57 -0600

    insercion de combo box

--- ProyectoTallerSoftware/Modulos/Hoja_Requisicion/Hoja_Requisicion.Designer.cs
+++ ProyectoTallerSoftware/Modulos/Hoja_Requisicion/Hoja_Requisicion.Designer.cs
@@ -48,7 +48,6 @@
             this.lbjose = new System.Windows.Forms.Label();
             this.lbUnicah = new System.Windows.Forms.Label();
             this.lbNombreP = new System.Windows.Forms.Label();
-            this.txtNombre = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -62,6 +61,7 @@
             this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbCerrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbInvent)).BeginInit();
@@ -304,9 +304,9 @@
             this.lbjose.Location = new System.Drawing.Point(266, 55);
             this.lbjose.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbjose.Name = "lbjose";
-            this.lbjose.Size = new System.Drawing.Size(242, 29);
+            this.lbjose.Size = new System.Drawing.Size(262, 29);
             this.lbjose.TabIndex = 6;
-            this.lbjose.Text = "Hoja de requisiones";
+            this.lbjose.Text = "Hoja de requisiciones";
             // 
             // lbUnicah
             // 
@@ -335,16 +335,6 @@
             this.lbNombreP.Size = new System.Drawing.Size(204, 22);
             this.lbNombreP.TabIndex = 19;
             this.lbNombreP.Text = "Unidad Demandante:";
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.BackColor = System.Drawing.Color.White;
-            this.txtNombre.Font = new System.Drawing.Font("Arial Narrow", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNombre.Location = new System.Drawing.Point(481, 169);
-            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(256, 27);
-            this.txtNombre.TabIndex = 20;
             // 
             // textBox1
             // 
@@ -381,7 +371,7 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(427, 504);
+            this.label2.Location = new System.Drawing.Point(391, 504);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 23);
             this.label2.TabIndex = 0;
@@ -470,6 +460,19 @@
             this.textBox3.Size = new System.Drawing.Size(328, 27);
             this.textBox3.TabIndex = 34;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Aseo",
+            "Jardineria",
+            "Mantenimiento",
+            "Papeleria"});
+            this.comboBox1.Location = new System.Drawing.Point(483, 172);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 21);
+            this.comboBox1.TabIndex = 35;
+            // 
             // Hoja_Requisicion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -477,6 +480,7 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1349, 729);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.dateTimePicker3);
@@ -490,7 +494,6 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.lbNombreP);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -533,7 +536,6 @@
         private System.Windows.Forms.Label lbjose;
         private System.Windows.Forms.Label lbUnicah;
         private System.Windows.Forms.Label lbNombreP;
-        private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
@@ -547,5 +549,6 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
